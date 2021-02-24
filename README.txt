eb2ffddf4f63a73497c103f13db3892ff029178b
Nesta <nguerrero@emergya.com>
2021-02-24 14:19:41 +0100

    Minimal Theme

---
+++ README.txt
@@ -0,0 +1,10 @@
+ABOUT Da Vinci Minimal
+-----------
+
+Da Vinvi Minimal is a Classy Sub-theme. 
+
+ABOUT DRUPAL THEMING
+--------------------
+
+See https://www.drupal.org/docs/8/theming for more information on Drupal
+theming.
---
+++ js/da-vinci.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Da Vinci Minimal Custom Code of the javascript behaviour.
+ */
+
+'use strict';
+
+(function ($) {
+  Drupal.behaviors.da_vinci_minimalTheme = {
+    attach: function (context) {
+
+    }
+  };
+})(jQuery);
